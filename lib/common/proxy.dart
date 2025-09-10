0c12b26c4faff1d2253551bfcf00e560391a0aaa
Mizolixx <153405801+mizoil@users.noreply.github.com>
2025-09-10 15:31:16 +1100

    Deletr

--- lib/common/proxy.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flclashx/common/system.dart';
-import 'package:proxy/proxy.dart';
-
-final proxy = system.isDesktop ? Proxy() : null;
--- lib/providers/controllers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flclashx/models/models.dart';
-import 'package:flclashx/enum/enum.dart';
-import 'package:flclashx/providers/providers.dart';
-import 'package:flclashx/state.dart';
